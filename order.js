489640517781796801d3e22c8c97d8eb8288a9fe
zhenhuiWang <zhenhui.wang520@gmail.com>
2017-12-18 17:07:27 +0800

    uuu

---
+++ order.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zhenhuiWang on 17/12/18.
+ *
+ * Title: Your order, please
+ *
+ * Function: Your task is to sort a given string. Each word in the String will contain a single number.
+ * This number is the position the word should have in the result.
+ *
+ * Exercise Link: https://www.codewars.com/kata/your-order-please/train/javascript
+ */
+
+let words = "is2 Thi1s T4est 3a";
+ function sortNumber(wordA, wordB) {
+      var numA = +(wordA.match(/\d+/g)[0]);
+      var numB = +(wordB.match(/\d+/g)[0]);
+      return numA - numB;
+ }
+
+ function order(words){
+   words.split(' ').sort(sortNumber).join(' ');
+ }
