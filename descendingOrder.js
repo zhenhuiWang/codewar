46f07dce7072e43f0b64aaada4e3b7d9cb6c5c85
zhenhuiWang <zhenhui.wang520@gmail.com>
2017-12-18 17:38:11 +0800

    uuuu

---
+++ descendingOrder.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zhenhuiWang on 17/12/18.
+ *
+ * Title: Descending Order
+ *
+ * Function: Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order.
+ * Essentially, rearrange the digits to create the highest possible number.
+ *
+ * https://www.codewars.com/kata/descending-order/train/javascript
+ */
+
+ function descendingOrder(n){
+     let nn = `${n}`.split('').sort().reverse().join('');
+     console.log(parseInt(nn));
+     return nn;
+ }
+ descendingOrder(1021);
---
+++ scramble.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zhenhuiWang on 17/12/18.
+ *
+ * Title: Scramblies
+ *
+ * Function: Write function scramble(str1,str2) that returns true if a portion of str1 characters can be rearranged to match str2,
+ * otherwise returns false.
+ *
+ * https://www.codewars.com/kata/scramblies/train/javascript
+ */
+
+ function scramble(str1, str2) {
+     var count = {};
+     var arr1 = str1.split('');
+     var arr2 = str2.split('');
+     arr1.forEach(function(item) {
+         count[item] = (count[item] || 0) + 1;
+     });
+
+     return arr2.every(function(item) {
+         return count[item]--;
+     });
+ }
+ console.log(scramble('javscriptsss','javascriptt'));
