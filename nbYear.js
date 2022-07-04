865017f41b3c54b25413f3ab7bfbdbf60bd64888
zhenhuiWang <zhenhui.wang520@gmail.com>
2022-07-04 09:10:33 +0800

    u

--- nbYear.js
+++ nbYear.js
@@ -8,12 +8,12 @@
  * Exercise Link: https://www.codewars.com/kata/growth-of-a-population/train/javascript
  */
 
- function nbYear(p0, percent, aug, p) {
-     if(p0 >= p) {
-         return 0;
-       } else {
-         console.log(nbYear(p0*(1+percent/100)+aug, percent, aug, p));
-         return 1+nbYear(p0*(1+percent/100)+aug, percent, aug, p);
-       }
- }
- console.log(nbYear(1500, 5, 100, 2000));
+function nbYear(p0, percent, aug, p) {
+  if (p0 >= p) {
+    return 0
+  } else {
+    console.log(nbYear(p0 * (1 + percent / 100) + aug, percent, aug, p))
+    return 1 + nbYear(p0 * (1 + percent / 100) + aug, percent, aug, p)
+  }
+}
+console.log(nbYear(1500, 5, 100, 2000))
